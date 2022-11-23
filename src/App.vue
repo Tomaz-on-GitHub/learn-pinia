67b92c2a3e25414dedb9a0c070bc7a935f827682
Tomaz_on_git <gumimixer@gmail.com>
2022-11-23 11:09:00 +0100

    create initial vue environment with Pinia and Router

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="app">
+<nav>
+        <RouterLink to="/">Home</RouterLink> |
+        <RouterLink to="/about">About</RouterLink>
+</nav>
+
+<RouterView />
+
+</div>
+
+
+
+
+  
+</template>
+
+<style scoped>
+.app {
+  text-align: center;
+}
+
+</style>
